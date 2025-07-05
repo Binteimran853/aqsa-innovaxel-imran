974d6711f111a06bdca6ae3b721dfa08edb4e050
Dev <dev@Devs-MacBook-Pro.local>
2025-07-05 13:14:51 +0500

    after update URL model for Url attribut to make it simple urlField

--- shortener/models.py
+++ shortener/models.py
@@ -6,7 +6,7 @@ def generate_shortcode(length=6):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
 class URL(models.Model):
-    url = models.URLField(unique=True)
+    url = models.URLField()
     shortCode = models.CharField(max_length=10, unique=True, default=generate_shortcode)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)  # ✅ auto-update on save()
